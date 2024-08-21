86aeb7ca88f2b2584f75c79af0a3f7d9bfa551e3
benj-lazaro <benjie.work@gmail.com>
2024-08-22 02:11:02 +0800

    Section 01 variables initial commit

---
+++ 01-primitive-types/01-variables/code/variables.py
@@ -0,0 +1,17 @@
+# Python interpreter allocates memory to store data
+# The variable references the address of that memory location
+student_count = 1000
+
+# Python interpreter accesses the memory location to read the stored data
+# Uses the built-in function print() to display the data on screen (terminal)
+print(student_count)
+
+# Supported data types in a variable
+# Numbers (floating)
+rating = 4.99
+
+# Boolean
+is_published = True
+
+# String
+course_name = "Python programming"
